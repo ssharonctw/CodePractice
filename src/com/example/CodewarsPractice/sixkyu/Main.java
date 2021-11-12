9abe0805e8fc43bc6609500b2d80275295f67f92
ssharonctw <ssharonctw@hotmail.com>
2021-11-13 00:33:11 +0800

    added two new line in main

--- src/com/example/CodewarsPractice/sixkyu/Main.java
+++ src/com/example/CodewarsPractice/sixkyu/Main.java
@@ -2,6 +2,8 @@ package com.example.CodewarsPractice.sixkyu;
 
 public class Main {
     public static void main(String[] args){
+
+        
         System.out.println("Multiple3or5: " + new Multiple3or5().solution(10));
         System.out.println("Multiple3or5: " + new Multiple3or5().bestSolution(10));
         System.out.println("ConvertStringToCamelCase: " + new ConvertStringToCamelCase().solution("north-up-Red-Blue-mountain-Red-Samurai"));
