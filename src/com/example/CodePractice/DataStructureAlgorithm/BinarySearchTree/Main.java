06d850069e00862607575c167f6d1e5c5f951bf8
ssharonctw <ssharonctw@hotmail.com>
2022-01-05 15:07:21 +0800

    added Binary Search Tree description in it's main

--- src/com/example/CodePractice/DataStructureAlgorithm/BinarySearchTree/Main.java
+++ src/com/example/CodePractice/DataStructureAlgorithm/BinarySearchTree/Main.java
@@ -1,5 +1,15 @@
 package com.example.CodePractice.DataStructureAlgorithm.BinarySearchTree;
 
+//Binary Search Tree = A tree data structure, where each node is greater than it's left child,
+//		               but less than it's right.
+
+//					   benefit: easy to locate a node when they are in this order
+
+//					   time complexity: best case  O(log n)
+//									 worst case O(n)
+
+//					   space complexity: O(n)
+
 public class Main {
     public static void main(String[] args){
         BinarySearchTree tree = new BinarySearchTree();
